893c10a03a0cb084607795256da842fd01cd24db
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-20 09:47:23 -0500

    Added piece images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Board, PieceType, SquareColor } from "./components/types";
+import { Board, PieceColor, PieceType, SquareColor } from "./components/types";
 
 export default function Home() {
 
@@ -10,7 +10,9 @@ export default function Home() {
       <div className="grid gap-0" style={{ gridTemplateColumns: "repeat(8, 60px)", gridTemplateRows: "repeat(8, 60px)" }}>
 
         {Array.from({ length: 64 }).map((_, index) => (
-          <button key={index} onClick={(e) => {console.log(board.getSquareByIndexBackwards(index))}} className={`w-[60px] h-[60px] grid place-items-center font-bold ${board.getSquareByIndexBackwards(index)?.color == SquareColor.Light ? "bg-white text-black" : "bg-green-700 text-white" }`}>{board.getSquareByIndexBackwards(index)?.piece?.pieceType || null}</button>
+          <button key={index} onClick={(e) => {console.log(board.getSquareByIndexBackwards(index))}} className={`w-[60px] h-[60px] grid place-items-center font-bold ${board.getSquareByIndexBackwards(index)?.color == SquareColor.Light ? "bg-white text-black" : "bg-green-700 text-white" }`}>
+            {!board.getSquareByIndexBackwards(index)?.piece ? "" : <img src={`./${board.getSquareByIndexBackwards(index)?.piece?.pieceType.toLowerCase()}-${board.getSquareByIndexBackwards(index)?.piece?.pieceColor.toLowerCase()}.png`}></img>}
+          </button>
         ))}
         
       </div>
